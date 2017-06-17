e4b38a5dded75de51f77123f78b81f01a034549c
belenchura <belenchura@gmail.com>
2017-06-17 05:57:26 -0300

    Update PulpFiction.pl

--- PulpFiction.pl
+++ PulpFiction.pl
@@ -20,6 +20,30 @@ trabajaPara(Empleador,bernardo):-
   
 trabajaPara(Empleador,george):-
   saleCon(bernardo,Empleador).
+  
+esFiel(Persona):-
+	saleCon(Persona,Pareja),
+	findall(Pareja,saleCon(Persona,Pareja),Parejas),
+	length(Parejas,CuantasParejas),
+	CuantasParejas=1.
+ 
+ esPeligroso(Alguien):-
+	actPeligrosa(Alguien).
+esPeligroso(Alguien):-
+	jefePeligroso(Alguien).
+
+actPeligrosa(Alguien):-
+	personaje(Alguien,ladron(cosasRobadas)),
+	elem(licorerias,cosasRobadas).
+	
+actPeligrosa(Alguien):-
+	personaje(Alguien,mafioso(cargoMafioso)).
+	elem(maton,cargoMafioso).
+
+jefePeligroso(Alguien):-
+	trabajaPara(JefePeligroso,Alguien),
+	esPeligroso(JefePeligroso).
+ 
 
 
 
